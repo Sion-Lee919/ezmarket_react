310b1e42aa650d1506a03f1d5e71b9e9395bc6b1
minsu <minsur0919@gmail.com>
2025-02-04 14:26:00 +0900

    검색기능(현재는 이름[name] 기준) => SearchComponent 추가

--- react_ezmarket/src/components/HeaderComponent.js
+++ react_ezmarket/src/components/HeaderComponent.js
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
+import { SearchComponent } from "./SearchComponent";
 
 const HeaderComponent = (prop) => {
     return (
     <div>
+        <Link to="/">이지마켓</Link><br/>
         <Link to="/cart">장바구니</Link>
+        <SearchComponent></SearchComponent>
+
     </div>
     )
 }
