f75175006e00bbc7dd473b741f3f2f7455d9bb0b
minsu <minsur0919@gmail.com>
2025-02-04 09:26:54 +0900

    ItemDetail.js 추가

--- react_ezmarket/src/App.js
+++ react_ezmarket/src/App.js
@@ -1,7 +1,10 @@
 //import './App.css';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import CartList from './components/CartList';
+import HeaderComponent from './components/HeaderComponent';
+import ItemDetail from './components/ItemDetail';
 
 
 function App() {
@@ -9,11 +12,11 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-      
+      <HeaderComponent/>
       <Routes>
-        
+        <Route path='/cart' element={<CartList/>}/>
+        <Route path='/item/:itemid' element={<ItemDetail/>}/>
       </Routes>
-     
       </BrowserRouter>
     </div>
   );
---
+++ react_ezmarket/src/components/HeaderComponent.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const HeaderComponent = (prop) => {
+    return (
+    <div>
+        <Link to="/cart">장바구니</Link>
+    </div>
+    )
+}
+
+export default HeaderComponent;
