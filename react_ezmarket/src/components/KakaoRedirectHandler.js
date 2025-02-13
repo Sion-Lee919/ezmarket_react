db96a3bf5f3781cd455c2ca7346a41cf363eaa50
wendys1205 <wendys1205@naver.com>
2025-02-13 17:50:58 +0900

    카카오 로그인 api

--- react_ezmarket/src/components/KakaoRedirectHandler.js
+++ react_ezmarket/src/components/KakaoRedirectHandler.js
@@ -10,16 +10,23 @@ const KakaoRedirectHandler = () => {
     if (code) {
       console.log("받은 카카오 인가 코드:", code);
 
-      fetch("http://localhost:9090/api/login", {
+      fetch("http://localhost:9090/api/login/kakao", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ code }),
       })
         .then((res) => res.json())
         .then((data) => {
-          console.log("로그인 성공:", data);
-          alert("로그인 성공! 사용자 정보를 확인하세요.");
-          window.location.href = "/";
+          if (data.token) {
+            console.log("JWT 토큰 수신:", data.token);
+
+            localStorage.setItem("jwt", data.token);
+
+            alert("로그인 성공! 쇼핑몰 서비스 이용 가능");
+            window.location.href = "/cart";  
+          } else {
+            throw new Error("JWT 토큰 없음");
+          }
         })
         .catch((err) => {
           console.error("로그인 에러:", err);
@@ -32,7 +39,7 @@ const KakaoRedirectHandler = () => {
     }
   }, []);
 
-  if (loading) return <div>로그인 처리 중...</div>;
+  if (loading) return <div>로그인 처리 중</div>;
   if (error) return <div>로그인 실패: {error}</div>;
 
   return null;
