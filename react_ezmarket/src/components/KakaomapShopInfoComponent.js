c0efdaae2f8df262635f426b0ba174134c470f4b
minsu <minsur0919@gmail.com>
2025-02-25 17:50:00 +0900

    카카오맵 기능 수정중

--- react_ezmarket/src/components/KakaomapShopInfoComponent.js
+++ react_ezmarket/src/components/KakaomapShopInfoComponent.js
@@ -1,13 +1,23 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 const KakaomapShopInfoComponent = () => {
-    //let lat = 37.6112321;
-    //let lng = 126.912321321;
+
     const [location, setLocation] = useState({ latitude: null, longitude: null });
-    const [error, setError] = useState(null);      
-    
+    const [error, setError] = useState(null);
+    const [brandAddr, setBrandAddr] = useState([]);
 
     useEffect(() => {
+
+        axios({
+            url : `http://localhost:9090/brandaddress`,
+            method : 'GET',
+
+        })
+        .then(function(res){
+            setBrandAddr(res.data);
+        })
+
         if (navigator.geolocation) {
 
             const options = {
@@ -39,52 +49,49 @@ const KakaomapShopInfoComponent = () => {
         if (location){
             kakaomap(location);
         }        
-    }, [location])
+    })
 
     const kakaomap=(location) => {
+
+        console.log("kakaomap 실행중")
 
         var infowindow = new window.kakao.maps.InfoWindow({zIndex:1});
 
         var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
             mapOption = {
             center: new window.kakao.maps.LatLng(location.latitude, location.longitude), // 지도의 중심좌표
-            level: 3 // 지도의 확대 레벨
+            level: 10 // 지도의 확대 레벨
         };   
 
         // 지도를 생성합니다    
         var map = new window.kakao.maps.Map(mapContainer, mapOption); 
 
-        // 장소 검색 객체를 생성합니다
-        var ps = new window.kakao.maps.services.Places(map);
+        // 주소-좌표 변환 객체를 생성합니다
+        var gc = new window.kakao.maps.services.Geocoder(map);
 
-        // 카테고리로 편의점을 검색합니다
-        ps.categorySearch('CS2', placesSearchCB, {useMapBounds:true,useMapCenter:true}); 
+        // 주소로 좌표를 검색합니다
+        brandAddr.map(addr => (
+            gc.addressSearch(addr,placesSearchCB)
+        ));
 
         // 키워드 검색 완료 시 호출되는 콜백함수 입니다
         function placesSearchCB (data, status) {
-
-
 
             if (status === window.kakao.maps.services.Status.OK) {
                 const home = {y:location.latitude, x:location.longitude};
                 var circle = new window.kakao.maps.Circle({
                     center : new window.kakao.maps.LatLng(home?.y, home.x),  // 원의 중심좌표 입니다 
-                    radius: 20, // 미터 단위의 원의 반지름입니다 
+                    radius: 1000, // 미터 단위의 원의 반지름입니다 
                     strokeWeight: 3, // 선의 두께입니다 
                     strokeColor: '#75B8FA', // 선의 색깔입니다
                     strokeOpacity: 1, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
                     strokeStyle: 'dashed', // 선의 스타일 입니다
                     fillColor: '#CFE7FF', // 채우기 색깔입니다
-                    fillOpacity: 0.7  // 채우기 불투명도 입니다
+                    fillOpacity: 0.1  // 채우기 불투명도 입니다
                 });
                 circle.setMap(map);
-                //displayMarker(home);
                 for (var i=0; i<data.length; i++) {
-
                     displayMarker(data[i]);
-                    if (i === 1){
-                        console.log(data[i]);
-                    }
                 }       
             }
         }
@@ -100,10 +107,19 @@ const KakaomapShopInfoComponent = () => {
             });
 
             // 마커에 클릭이벤트를 등록합니다
-            window.kakao.maps.event.addListener(marker, 'click', function() {
+            window.kakao.maps.event.addListener(marker, 'mouseover', function() {
                 // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
-                infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + '</div>');
+                infowindow.setContent('<div style="width:200px;height:200px;padding:40px;font-size:40px;border-radius:8px">' + place?.address.address_name + '</div>');
                 infowindow.open(map, marker);
+            });
+
+            window.kakao.maps.event.addListener(marker, 'mouseout', function() {
+                // 마커에 마우스아웃 이벤트가 발생하면 인포윈도우를 제거합니다
+                infowindow.close();
+            });
+
+            window.kakao.maps.event.addListener(marker, 'click', function() {
+                // 마커를 클릭하면 해당 브랜드주소를 가진 곳으로 이동            
             });
         }
 
