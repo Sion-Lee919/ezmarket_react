15baeb104ee19f0e1554b19eb462a7b26bd89a3d
Hyeon5 <hyeonjinn0301@gmail.com>
2025-02-28 15:14:59 +0900

    내정보 임시 디자인 추가

--- react_ezmarket/src/pages/member/my/myPage.js
+++ react_ezmarket/src/pages/member/my/myPage.js
@@ -40,25 +40,111 @@ const MyPage = () => {
     }
   }, [navigate]);
 
-  const handleModify = () => {
-    navigate(`/my/modify?username=${user.username}`);
-  }
-
   const handleAdminPageClick = () => {
     navigate(`/my/admin`);
   }
 
+  const handleSellerPageClick = () => {
+    navigate(`/brand/${user.brand_id}`);
+  }
+
   return (
     <div className="mypage-form">
-      <MyPageSideBar />
-      <div>
-        <div>{user.realname}님은 {user.author === 0 ? "관리자" : user.userauthor === 2 ? "판매자" : "일반회원"}입니다.</div>
-        <div>적립금: {user.points}원</div>
-        <div>충전금: {user.ezpay}원</div>
-      </div>
-      <div className="side-bar">
-      <div>
-          {user.username}의 주문 내역을 보여줄 계획
+      <MyPageSideBar></MyPageSideBar>
+      <div className="mypage-info">
+        <div className="mypage-box-info">
+          {user.userauthor === 0 && (
+          <button className="admin-page-button" onClick={handleAdminPageClick}>관리자 페이지</button>
+          )}
+          {user.userauthor === 2 && (
+          <button className="seller-page-button" onClick={handleSellerPageClick}>판매자 페이지</button>
+          )}
+          {user.realname}님은 <br></br> {user.userauthor === 0 ? "관리자" : user.userauthor === 2 ? "판매자" : "일반회원"}입니다<br></br>
+        </div>
+        <div className="mypage-box-info">
+          <img src="/images/tempo1.png" alt="임시 이미지1" className="point-image"></img>
+          <div>적립금</div>
+          <div><span className="mypage-point">{user.points}</span>원</div>
+        </div>
+        <div className="mypage-box-info">
+        <img src="/images/tempo2.png" alt="임시 이미지2" className="mileage-image"></img>
+          <div>충전금</div> 
+          <div><span className="mypage-money">{user.ezpay}</span>원</div>
+        </div>
+
+        <div className="mypage-box-order-flow">
+          {user.username}의 주문 내역 진행 상황을 보여줄 계획
+          <div className="order-flow-detail">
+              <div>
+                <div>결제 완료</div>
+                <div className="order-flow-box">
+                  숫자
+                </div>
+              </div>
+              <div>
+                <div>상품 준비중</div>
+                <div className="order-flow-box">
+                  숫자
+                </div>
+              </div>
+              <div>
+                <div>배송중</div>
+                <div className="order-flow-box">
+                  숫자
+                </div>
+              </div>
+              <div>
+                <div>배송 완료</div>
+                <div className="order-flow-box">
+                  숫자
+                </div>
+              </div>
+          </div> 
+        </div>
+        <div className="mypage-box-order-image">
+          {user.username}의 최근 주문 내역을 이미지로 5개 보여주고, 누르면 상품페이지로 이동할 계획
+          <div className="order-image-detail">
+            <div>
+              <div>상품1</div>
+              <div className="order-image-box">
+                <a href="localhost:3000/item/brand-url">
+                  <img src="brand-image-url" alt="최근 1번"></img>
+                </a>
+              </div>
+            </div>
+            <div>
+              <div>상품2</div>
+              <div className="order-image-box">
+                <a href="localhost:3000/item/brand-url">
+                  <img src="brand-image-url" alt="최근 2번"></img>
+                </a>
+              </div>
+            </div>
+            <div>
+              <div>상품3</div>
+              <div className="order-image-box">
+                <a href="localhost:3000/item/brand-url">
+                  <img src="brand-image-url" alt="최근 3번"></img>
+                </a>
+              </div>
+            </div>
+            <div>
+              <div>상품4</div>
+              <div className="order-image-box">
+                <a href="localhost:3000/item/brand-url">
+                  <img src="brand-image-url" alt="최근 4번"></img>
+                </a>
+              </div>
+            </div>
+            <div>
+              <div>상품5</div>
+              <div className="order-image-box">
+                <a href="localhost:3000/item/brand-url">
+                  <img src="brand-image-url" alt="최근 5번"></img>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
