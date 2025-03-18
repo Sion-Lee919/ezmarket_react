cb9a2d590df3c178ea5d84da5f1e15ba856418e4
Hyeon5 <hyeon5@github.com>
2025-03-18 19:48:40 +0900

    소셜로그인 전화번호 업데이트 alert

--- react_ezmarket/src/pages/member/main.js
+++ react_ezmarket/src/pages/member/main.js
@@ -2,8 +2,40 @@ import { useNavigate } from 'react-router-dom';
 import ItemSlideComponent from '../../components/ItemSlideComponent';
 import KakaomapShopInfoComponent from '../../components/KakaomapShopInfoComponent';
 import MainBannerSlide from "../../components/MainBannerSlide";
+import { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
+import axios from 'axios';
+
+const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:9090";
 
 const Main = () => {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = Cookies.get('jwt_token'); 
+    
+    if (token) {
+      axios.get(`${API_BASE_URL}/userinfo`, { 
+        headers: { 'Authorization': `Bearer ${token}` }, 
+        withCredentials: true
+      })
+      .then(response => {
+        setUser(response.data);
+        if(response.data.phone === "010-0000-0000") {
+          alert('소셜로그인 사용자는 전화번호 업데이트가 필요합니다!')
+          navigate('/my/modify');
+        }
+      })
+      .catch(error => {
+        alert(error.response.data.message);
+        Cookies.remove('jwt_token');
+        navigate('/login');
+      });
+    } else {
+      navigate('/login');
+    }
+  }, [navigate]);
 
   return (
     <div>
